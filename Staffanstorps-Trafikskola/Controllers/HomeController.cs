027853561d1dcbeb80a01256742a1c1ec946eddd
kushtrim <kushtrim_shabooni99@hotmail.com>
2025-02-15 17:54:55 +0100

    Add project files.

---
+++ Staffanstorps-Trafikskola/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Staffanstorps_Trafikskola.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [Route("/Contact")]
+    public IActionResult Contact()
+    {
+        return View();
+    }
+
+    [Route("/About")]
+    public IActionResult About()
+    {
+        return View();
+    }
+}
---
+++ Staffanstorps-Trafikskola/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿const hamburger = document.getElementById('hamburger-icon');
+const mobileMenu = document.getElementById('mobile-menu');
+
+
+hamburger.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+    hamburger.classList.toggle('active');
+});
+
+
+mobileMenu.addEventListener('click', (e) => {
+    if (e.target === mobileMenu) {
+        mobileMenu.classList.remove('active');
+        hamburger.classList.remove('active');
+    }
+});
